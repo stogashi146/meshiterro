f07599ab0d13d0f15db2a9b28518bf9e6455d982
stogashi146 <stogashi146@gmail.com>
2021-06-30 06:50:41 +0000

    [Finish] meshiterro-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '9b985c7dd8618c60a9472d43830311d7ffee448a1606aa11be028990bdfa93bade7cca4be3a3dd853f837c5009cb3f5277df6e7f718e2cdd65edaf9a0793386e'
